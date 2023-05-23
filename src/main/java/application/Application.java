f968d7ba396739353ab241a34afd7bde69169292
LeoValleWeb <leomartinsvalle@gmail.com>
2023-05-23 14:19:38 +0000

    save

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,7 +1,11 @@
-public class Application {
+package main.java.application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
+public class Application {
     public static void main(String[] args) {
-        
-    }
-    
+        SpringApplication.run(Application.class, args);
+    }  
 }
